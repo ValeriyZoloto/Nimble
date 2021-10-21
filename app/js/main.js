eb83e658be6cbfdf0f1f0aa29e573adeba48d74b
Valeriy <leshikzoloto@gmail.com>
2021-10-21 18:53:30 +0300

    header-ready desctop and mobile start mob-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-$(function () {});
+$(function () {
+	//авто-закрытие мобильного меню
+	$(".header__menu-open, .menu__item a").on("click", function () {
+		$(".menu").toggleClass("menu--active"),
+			$(".header__menu-open").toggleClass("header__menu-open--active");
+	});
+});
