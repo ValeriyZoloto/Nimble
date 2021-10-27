076dbcfdf95aa878611063d7ae8578f0aaed86ab
Valeriy <leshikzoloto@gmail.com>
2021-10-27 19:36:22 +0300

    add staking-desctop

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,7 @@ $(function () {
 	$(".header__menu-open, .menu__item a").on("click", function () {
 		$(".menu").toggleClass("menu--active"),
 			$(".header__menu-open").toggleClass("header__menu-open--active");
+		$(".revolution__title").toggleClass("revolution__title--active");
 	});
 
 	//revolution-slider
@@ -37,5 +38,67 @@ $(function () {
 				},
 			},
 		],
+	});
+
+	//staking__slider
+	$(".staking__slider").slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		infinite: true,
+
+		responsive: [
+			{
+				breakpoint: 1100,
+				settings: {
+					slidesToShow: 4,
+				},
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 3,
+					prevArrow:
+						'<button type="button" class="slick-prev"><svg width="34" height="10" viewBox="0 0 34 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.00003 0L6.50216 1.5022L4.06662 3.93774H33.723V6.06219H4.06662L6.50216 8.4978L5.00003 10L-3.8147e-06 4.99996L5.00003 0Z" fill="#B4F299"/></svg></button>',
+
+					nextArrow:
+						'<button type="button" class="slick-next"><svg width="34" height="10" viewBox="0 0 34 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28.723 0L27.2209 1.5022L29.6564 3.93774H0V6.06219H29.6564L27.2209 8.4978L28.723 10L33.723 4.99996L28.723 0Z" fill="#B4F299"/></svg></button>',
+				},
+			},
+			{
+				breakpoint: 582,
+				settings: {
+					slidesToShow: 2,
+					prevArrow:
+						'<button type="button" class="slick-prev"><svg width="34" height="10" viewBox="0 0 34 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.00003 0L6.50216 1.5022L4.06662 3.93774H33.723V6.06219H4.06662L6.50216 8.4978L5.00003 10L-3.8147e-06 4.99996L5.00003 0Z" fill="#B4F299"/></svg></button>',
+
+					nextArrow:
+						'<button type="button" class="slick-next"><svg width="34" height="10" viewBox="0 0 34 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28.723 0L27.2209 1.5022L29.6564 3.93774H0V6.06219H29.6564L27.2209 8.4978L28.723 10L33.723 4.99996L28.723 0Z" fill="#B4F299"/></svg></button>',
+				},
+			},
+		],
+	});
+
+	$(".token__btn").on("click", function () {
+		$(".token__text--hidden").addClass("token__text--active");
+		$(".token__btn").addClass("token__btn--hidden");
+		$(".token__botton").addClass("token__botton--open");
+	});
+
+	$(".token__botton").on("click", function () {
+		$(".token__text--hidden").removeClass("token__text--active");
+		$(".token__botton").removeClass("token__botton--open");
+		$(".token__btn").removeClass("token__btn--hidden");
+	});
+
+	$(".staking__btn").on("click", function () {
+		$(".staking__text--hidden").addClass("staking__text--active");
+		$(".staking__btn").addClass("staking__btn--hidden");
+		$(".staking__botton").addClass("staking__botton--open");
+	});
+
+	$(".staking__botton").on("click", function () {
+		$(".staking__text--hidden").removeClass("staking__text--active");
+		$(".staking__botton").removeClass("staking__botton--open");
+		$(".staking__btn").removeClass("staking__btn--hidden");
 	});
 });
